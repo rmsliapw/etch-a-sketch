e018bdf9758931490394b2dbf4c972089b5e8abb
rmsliapw <rmsliapw@gmail.com>
2023-02-06 14:29:33 +0300

    Add custom number grid generation

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
 'use strict';
 
 const grid = document.querySelector('.grid');
+const input = document.querySelector('#input');
+const btn = document.querySelector('.btn');
 
-const cell = document.createElement('div');
-cell.classList.add('grid-cell');
+btn.addEventListener('click', () => {
+  const value = getValue();
+  if (value) {
+    generateGrid(value);
+  }
+});
 
-// for (let i = 0; i < 257; i++) {
-//   grid.append(cell.cloneNode(true));
-// }
+// First Load
+generateGrid();
+
+function getValue() {
+  console.log(input.value);
+  return +input.value;
+}
+
+function generateGrid(inputValue = 16) {
+  const cell = document.createElement('div');
+  cell.classList.add('grid-cell');
+  cell.style.width = (640 / inputValue) + "px";
+  cell.style.height = (640 / inputValue) + "px";
+  console.log(cell.width, cell.height);
+  for (let i = 0; i < (inputValue ** 2); i++) {
+    grid.append(cell.cloneNode(true));
+  }
+}
+
