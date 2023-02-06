fe0489c235f7561c643cf84a487f7dd5d0ca7764
rmsliapw <rmsliapw@gmail.com>
2023-02-06 11:36:20 +0300

    Add html structure and styles

---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const grid = document.querySelector('.grid');
+
+const cell = document.createElement('div');
+cell.classList.add('grid-cell');
+
+// for (let i = 0; i < 257; i++) {
+//   grid.append(cell.cloneNode(true));
+// }
